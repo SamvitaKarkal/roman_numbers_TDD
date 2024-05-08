6879b54511a3b6d165a307a08768a9626a4ec23c
samvitakarkal <samvitaskarkal@gmail.com>
2024-05-08 12:51:02 +0530

    Added test cases for items in hash

--- spec/test_roman_numerals_spec.rb
+++ spec/test_roman_numerals_spec.rb
@@ -19,5 +19,55 @@ RSpec.describe RomanNumerals do
       roman_number = roman_numerals.string_convert(3)
       expect(roman_number).to eq("III")
     end
+
+    it "checks if 4 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(4)
+      expect(roman_number).to eq("IV")
+    end
+
+    it "checks if 5 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(5)
+      expect(roman_number).to eq("V")
+    end
+
+    it "checks if 9 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(9)
+      expect(roman_number).to eq("IX")
+    end
+
+    it "checks if 10 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(10)
+      expect(roman_number).to eq("X")
+    end
+
+    it "checks if 21 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(21)
+      expect(roman_number).to eq("XXI")
+    end
+
+    it "checks if 50 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(50)
+      expect(roman_number).to eq("L")
+    end
+
+    it "checks if 50 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(50)
+      expect(roman_number).to eq("L")
+    end
+
+    it "checks if 100 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(100)
+      expect(roman_number).to eq("C")
+    end
+
+    it "checks if 500 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(500)
+      expect(roman_number).to eq("D")
+    end
+
+    it "checks if 1000 is converted into roman number" do
+      roman_number = roman_numerals.string_convert(1000)
+      expect(roman_number).to eq("M")
+    end
   end
 end
