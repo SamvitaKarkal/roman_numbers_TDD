11cd69bfea26e67aac036eb3f1feff1a46d6fd4a
samvitakarkal <samvitaskarkal@gmail.com>
2024-05-08 12:43:34 +0530

    Updated code to handle roman numbers from list

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,4 +1,25 @@
+require 'byebug'
+
 class RomanNumerals
-  def string_convert(int)
+  def initialize
+    @decimal_to_roman = {
+      1 => "I",
+      2 => "II",
+      3 => "III",
+      4 => "IV",
+      5 => "V",
+      9 => "IX",
+      21 => "XXI",
+      50 => "L",
+      100 => "C",
+      500 => "D",
+      1000 => "M"
+    }
+  end
+
+  def string_convert(input)
+    if @decimal_to_roman.include?(input)
+      return @decimal_to_roman[input]
+    end
   end
 end
